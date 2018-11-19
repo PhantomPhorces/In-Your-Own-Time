57fae499086f6dff3fc044ddab4d520981525745
PhantomPhorces <garethcockburn@crawfordcloud.co.za>
2018-11-19 15:46:55 +0200

    worked on centering home

--- home.js
+++ home.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var dwidth = $("#taskbar").width();
     var twidth = $("#taskbar").children("a").width();
-    $("#test").html( twidth);
+    $("#taskbar").children("a").css("left", (dwidth + twidth) / 2);
+    $("#taskbar").children("a").css("margin-left", twidth *-1);
+});
+$(window).resize(function () {
+    var dwidth = $("#taskbar").width();
+    var twidth = $("#taskbar").children("a").width();
+    $("#taskbar").children("a").css("left", (dwidth + twidth) / 2);
+    $("#taskbar").children("a").css("margin-left", twidth *-1);
 });
