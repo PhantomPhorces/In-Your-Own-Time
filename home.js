a8dc512f64f087c217ee24823f84f6183c2d480a
PhantomPhorces <garethcockburn@crawfordcloud.co.za>
2018-11-19 16:05:40 +0200

    alligned vertically

--- home.js
+++ home.js
@@ -3,10 +3,18 @@ $(document).ready(function () {
     var twidth = $("#taskbar").children("a").width();
     $("#taskbar").children("a").css("left", (dwidth + twidth) / 2);
     $("#taskbar").children("a").css("margin-left", twidth *-1);
+    var dheight = $("#taskbar").height();
+    var theight = $("#taskbar").children("a").height();
+    $("#taskbar").children("a").css("top", (dheight + theight) / 2);
+    $("#taskbar").children("a").css("margin-top", theight *-1);
 });
 $(window).resize(function () {
     var dwidth = $("#taskbar").width();
     var twidth = $("#taskbar").children("a").width();
     $("#taskbar").children("a").css("left", (dwidth + twidth) / 2);
     $("#taskbar").children("a").css("margin-left", twidth *-1);
+    var dheight = $("#taskbar").height();
+    var theight = $("#taskbar").children("a").height();
+    $("#taskbar").children("a").css("top", (dheight + theight) / 2);
+    $("#taskbar").children("a").css("margin-top", theight *-1);
 });
