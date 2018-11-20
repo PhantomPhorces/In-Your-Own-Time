29b322ca838b1df43978879eb3cb714e1687d7ee
PhantomPhorces <garethcockburn@crawfordcloud.co.za>
2018-11-20 22:02:45 +0200

    added other links and centered

--- home.js
+++ home.js
@@ -1,20 +1,16 @@
 $(document).ready(function () {
-    var dwidth = $("#taskbar").width();
-    var twidth = $("#taskbar").children("a").width();
-    $("#taskbar").children("a").css("left", (dwidth + twidth) / 2);
-    $("#taskbar").children("a").css("margin-left", twidth *-1);
-    var dheight = $("#taskbar").height();
-    var theight = $("#taskbar").children("a").height();
-    $("#taskbar").children("a").css("top", (dheight + theight) / 2);
-    $("#taskbar").children("a").css("margin-top", theight *-1);
+    center();
 });
 $(window).resize(function () {
-    var dwidth = $("#taskbar").width();
-    var twidth = $("#taskbar").children("a").width();
-    $("#taskbar").children("a").css("left", (dwidth + twidth) / 2);
-    $("#taskbar").children("a").css("margin-left", twidth *-1);
-    var dheight = $("#taskbar").height();
-    var theight = $("#taskbar").children("a").height();
-    $("#taskbar").children("a").css("top", (dheight + theight) / 2);
-    $("#taskbar").children("a").css("margin-top", theight *-1);
+    center();
 });
+function center(){
+    var dwidth = $("#taskbar").width();
+    var twidth = $("#links").width();
+    $("#links").css("left", (dwidth + twidth) / 2);
+    $("#links").css("margin-left", twidth *-1);
+    var dheight = $("#taskbar").height();
+    var theight = $("#links").height();
+    $("#links").css("top", (dheight + theight) / 2);
+    $("#links").css("margin-top", theight *-1);
+}
