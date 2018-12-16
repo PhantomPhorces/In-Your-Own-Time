6bf0d255acb6e358fe8f7f45ed05eb8986cdd450
PhantomPhorces <garethcockburn@crawfordcloud.co.za>
2018-12-16 12:17:45 +0200

    Why is it so damn hard to animate an underline!

--- home.js
+++ home.js
@@ -4,13 +4,19 @@ $(document).ready(function () {
 $(window).resize(function () {
     center();
 });
-function center(){
+
+function center() {
     var dwidth = $("#taskbar").width();
     var twidth = $("#links").width();
     $("#links").css("left", (dwidth + twidth) / 2);
-    $("#links").css("margin-left", twidth *-1);
+    $("#links").css("margin-left", twidth * -1);
     var dheight = $("#taskbar").height();
     var theight = $("#links").height();
     $("#links").css("top", (dheight + theight) / 2);
-    $("#links").css("margin-top", theight *-1);
+    $("#links").css("margin-top", theight * -1);
 }
+$(".nav").hover(function () {
+    $(".nav").animate({
+        borderBottom: "4px solid #3498db !important"
+    }, 1);
+});
